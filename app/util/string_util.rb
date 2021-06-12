2efc799a31c2bc5c6fa5b8923ba3ce364a1ddfd9
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2021-06-12 16:40:35 -0300

    Improvement in the method StringUtil.isInteger.

--- app/util/string_util.rb
+++ app/util/string_util.rb
@@ -1,6 +1,8 @@
 class StringUtil
 
+    # It returns true, if the value is an integer or a string with a integer value.
     def self.isInteger?( valueString )
+        return true if valueString.is_a?(Integer)
         return false if not valueString.is_a?(String)
         return valueString.to_i.to_s == valueString
     end
