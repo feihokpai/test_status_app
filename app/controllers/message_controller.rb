9514808d61e7329626875c34e4f68b7decde252b
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2021-06-12 16:41:21 -0300

    I added a new page where the user can save a new Status Message. * The functionality is working.

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -4,7 +4,7 @@ class MessageController < ApplicationController
   MAX_MESSAGES_TO_SHOW = 10
 
   def initialize
-    @errorMsg = ""
+    @messageToUser = ""
     @messageService ||= StatusMessagesService.new
     @statusMessageList ||= []
     @applicationService ||= ApplicationService.new
@@ -13,7 +13,32 @@ class MessageController < ApplicationController
   end
 
   def index
-    puts "Entrou no index"
+    load_messages_list()
+  end
+
+  def save
+    save_message_from_params()
+    load_messages_list()
+    render :index
+  end
+
+  private
+
+  def load_messages_list
     @statusMessageList = @messageService.get_messages( @defaultApplication, MAX_MESSAGES_TO_SHOW )
   end
+
+  def save_message_from_params
+    begin
+      statusCode = params[:status_code].to_i()
+      message = params[:status_message]
+      statusApplication = StatusApplication.fromCode( statusCode )
+      statusMessage = StatusMessage.new( @defaultApplication, statusApplication, message )
+      @messageService.save_status_message( statusMessage )
+    rescue StandardError => exception
+      @messageToUser = "An Unexpected error ocurred: #{exception.message}"
+      ExceptionUtil.printLinesOfBackTrace( exception, 10 )
+    end
+  end
+
 end
