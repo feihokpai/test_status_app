ce10241265462568728d2a2513f6de1ff147c9c5
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2021-06-12 14:06:38 -0300

    I fixed a bug in StatusMessage that didn't allow setting the id to nil.

--- app/domain/status_message.rb
+++ app/domain/status_message.rb
@@ -2,12 +2,12 @@ class StatusMessage < DomainObject
   attr_reader :message, :timestamp, :application, :status, :id
 
   def initialize( application_, status_, message_, timestamp_=Time.now, id_=nil)
-    validate_initialize( application_, timestamp_, id_)
-    @id = id_
-    @timestamp = timestamp_    
-    @application = application_
+    validate_initialize( application_, timestamp_, id_)    
+    self.id=(id_)
     self.message=(message_)
     self.status=(status_)
+    @timestamp = timestamp_    
+    @application = application_
   end
 
   def message=(message_)
@@ -20,12 +20,14 @@ class StatusMessage < DomainObject
     @status = status_
   end
 
+  def id=( id_ )
+    ValidateUtil.raiseIfValueIsNotNilAndIsNotA( id_, Integer )
+    @id = id_
+  end
+
   private
 
   def validate_initialize( application_, timestamp_, id_)
-    if !id_.nil?
-      ValidateUtil.raiseIfValueIsNotA( id_, Integer )
-    end
     ValidateUtil.raiseIfValueIsNotA( application_, Application )
     ValidateUtil.raiseIfValueIsNotA( timestamp_, Time )    
   end
