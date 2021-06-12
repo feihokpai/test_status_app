3b782a040f62158d45e5df36b08f0856d9e5cf68
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2021-06-12 14:06:00 -0300

    I added the field description in StatusApplication.

--- app/domain/status_application.rb
+++ app/domain/status_application.rb
@@ -1,25 +1,31 @@
 class StatusApplication < DomainObject
-  attr_reader :code
+  attr_reader :code, :description
 
   STATUS_DOWN = 99
   STATUS_UP = 100
 
-  def initialize( status_ )
-    validate_initialize( status_ )
+  def initialize( status_, description_ )
+    validate_initialize( status_, description_ )
     @code = status_
+    @description = description_
   end
 
   def self.up
-    StatusApplication.new( StatusApplication::STATUS_UP )
+    StatusApplication.new( StatusApplication::STATUS_UP, "UP" )
   end
 
   def self.down
-    StatusApplication.new( StatusApplication::STATUS_DOWN )
+    StatusApplication.new( StatusApplication::STATUS_DOWN, "DOWN" )
   end
 
   private
 
-  def validate_initialize( status_ )
+  def validate_initialize( status_, description_ )
+    raiseIfStatusIsNotValid( status_ )
+    StringUtil.raiseIfIsEmptyOrNil( description_ )
+  end
+
+  def raiseIfStatusIsNotValid( status_ )
     if not valid_status_list().include?( status_ )
       message = "Tried to create a Status Application with invalid code: #{status_}"
       raise ArgumentError.new( message )
