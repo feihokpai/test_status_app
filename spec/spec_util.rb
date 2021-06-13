a3985ad72f4451d90c4d9905796282d5a28bf246
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2021-06-13 08:49:54 -0300

    I created the tests to the domain class Application. * Added a new method in StringUtil to identify blank strings. * New methods in SpecUtil.

--- spec/spec_util.rb
+++ spec/spec_util.rb
@@ -1,9 +1,12 @@
+
 class SpecUtil
 
   class << self
+
     def notPassedWithMessage( msg )
-      puts "msg: #{msg}" 
-      expect( true ).to eq( false )
+      puts "msg: #{msg}"
+      raise StandardError.new( "error message: #{msg}" )
+      # expect( true ).to eq( false )
     end
     
     def notPassed( )
@@ -13,17 +16,36 @@ class SpecUtil
     def passed()
       expect( true ).to eq( true )
     end
+
+    def arrayWithObjectsFromManyTypes( exceptClass=nil)
+      types = []
+      types << 1
+      types << 1.3
+      types << "aaa"
+      types << StandardError.new
+      types << [1,2,3]
+      types << Time.now
+      types << {"a": 1, "b":2}
+      return types if exceptClass.nil?
+      types = types.select {|value| value.class != exceptClass}
+      return types
+    end
   
     def test_execution( block, itShouldPass )
       begin
+        msg = nil
         block.call()
         if( !itShouldPass )
-          notPassedWithMessage( "It was expected an error, but it didn't occur" )
+          msg = "It was expected an error, but it didn't occur"
         end
       rescue StandardError => exception
         if( itShouldPass )
-          notPassedWithMessage( "An unexpected error occurred." )
+          msg = "An unexpected error occurred: #{exception.message}"
         end
+      end
+
+      if !msg.nil?
+        notPassedWithMessage( msg ) 
       end
     end
   end  
